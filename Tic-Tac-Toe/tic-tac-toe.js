5509302c49e03b75c3b6cc4520f7f9b301c71754
William Florido <william.frdo@gmail.com>
2022-03-27 13:27:02 -0300

    Adding javascript

--- Tic-Tac-Toe/tic-tac-toe.js
+++ Tic-Tac-Toe/tic-tac-toe.js
@@ -1,5 +1,16 @@
 let player = null;
+let selectedPlayer = document.getElementById('selected-player');
+
+changePlayer('X');
 
 function pickSquare(id) {
-    console.log(id);
+    let square = document.getElementById(id);
+
+    square.innerHTML = player;
+    square.style.color = '#000';
+}
+
+function changePlayer(value) {
+    player = value;
+    selectedPlayer.innerHTML = player;
 }
