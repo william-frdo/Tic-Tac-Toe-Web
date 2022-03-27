f4861c0e3f2fd09deb3c5e903f11646a41b286ad
William Florido <william.frdo@gmail.com>
2022-03-27 13:38:40 -0300

    Change Player

--- Tic-Tac-Toe/tic-tac-toe.js
+++ Tic-Tac-Toe/tic-tac-toe.js
@@ -1,16 +1,33 @@
 let player = null;
 let selectedPlayer = document.getElementById('selected-player');
+let squares = document.getElementsByName('square');
 
 changePlayer('X');
 
 function pickSquare(id) {
     let square = document.getElementById(id);
 
+    if (square.innerHTML !== '-') {
+        return;
+    }
+
     square.innerHTML = player;
     square.style.color = '#000';
+
+    if (player === 'X') {
+        player = 'O';
+    } else {
+        player = 'X';
+    }
+
+    changePlayer(player);
 }
 
 function changePlayer(value) {
     player = value;
     selectedPlayer.innerHTML = player;
+}
+
+function checkWinner() {
+    
 }
